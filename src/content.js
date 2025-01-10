b7bb150c4bdefd99dbd6e6401c2c8fa7d7e1c6d7
synhya <synhya@yeouidopartners.com>
2025-01-10 23:19:17 +0900

    .

--- src/content.js
+++ src/content.js
@@ -1,15 +1,6 @@
 import github from "@actions/github";
 
 export default class Content {
-  /**
-   * @type {Object<string, string>}
-   */
-  authorMap = {
-    ariandel: "장원익",
-    "ariandel-dev": "장원익",
-    wldygus2023: "지요현",
-  };
-
   constructor() {
     this.values = github.context;
   }
@@ -36,137 +27,136 @@ export default class Content {
     // 커밋 URL
     const commitUrl = payload?.head_commit?.url || "#";
 
-    // authorMap에서 실제 이름을 가져오거나 기본 author 사용
-    const authorName = this.authorMap[author] || author;
+    const authorName = author;
 
     return {
       content: {
         type: "flex",
-          altText: `[${repository}]\n ${commitMessage}`,
-          contents: {
-            type: "carousel",
-            contents: [
-              {
-                type: "bubble",
-                size: "mega",
-                header: {
-                  type: "box",
-                  layout: "horizontal",
-                  contents: [
-                    {
-                      type: "text",
-                      text: repository,
-                      size: "lg",
-                      color: "#00c73c",
-                      weight: "bold",
-                    },
-                  ],
-                  backgroundColor: "#ffffff",
-                },
-                body: {
-                  type: "box",
-                  layout: "vertical",
-                  contents: [
-                    {
-                      type: "box",
-                      layout: "vertical",
-                      contents: [
-                        {
-                          type: "text",
-                          text: commitMessage,
-                          wrap: true,
-                          size: "md",
-                        },
-                      ],
-                      margin: "xl",
-                    },
-                    {
-                      type: "box",
-                      layout: "vertical",
-                      contents: [
-                        {
-                          type: "box",
-                          layout: "baseline",
-                          contents: [
-                            {
-                              type: "text",
-                              text: "Branch",
-                              size: "sm",
-                              color: "#858f89",
-                              flex: 3,
-                              margin: "md",
-                            },
-                            {
-                              type: "text",
-                              text: branch,
-                              size: "sm",
-                              margin: "md",
-                              flex: 5,
-                            },
-                          ],
-                        },
-                        {
-                          type: "box",
-                          layout: "baseline",
-                          contents: [
-                            {
-                              type: "text",
-                              text: "Author",
-                              size: "sm",
-                              color: "#858f89",
-                              flex: 3,
-                              margin: "md",
-                            },
-                            {
-                              type: "text",
-                              text: authorName,
-                              size: "sm",
-                              margin: "md",
-                              flex: 5,
-                            },
-                          ],
-                          margin: "sm",
-                        },
-                        {
-                          type: "box",
-                          layout: "baseline",
-                          contents: [
-                            {
-                              type: "text",
-                              text: "Commit",
-                              size: "sm",
-                              color: "#858f89",
-                              flex: 3,
-                              margin: "md",
-                            },
-                            {
-                              type: "text",
-                              text: commitUrl.split("/").pop() || "",
-                              size: "sm",
-                              margin: "md",
-                              flex: 5,
-                              decoration: "underline",
-                              action: {
-                                type: "uri",
-                                label: "",
-                                uri: commitUrl,
-                              },
-                            },
-                          ],
-                          margin: "sm",
-                        },
-                      ],
-                      margin: "xxl",
-                      width: "260px",
-                      alignItems: "center",
-                      paddingAll: "6px",
-                    },
-                  ],
-                },
+        altText: `[${repository}]\n ${commitMessage}`,
+        contents: {
+          type: "carousel",
+          contents: [
+            {
+              type: "bubble",
+              size: "mega",
+              header: {
+                type: "box",
+                layout: "horizontal",
+                contents: [
+                  {
+                    type: "text",
+                    text: repository,
+                    size: "lg",
+                    color: "#00c73c",
+                    weight: "bold",
+                  },
+                ],
+                backgroundColor: "#ffffff",
               },
-            ],
-          },
+              body: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "box",
+                    layout: "vertical",
+                    contents: [
+                      {
+                        type: "text",
+                        text: commitMessage,
+                        wrap: true,
+                        size: "md",
+                      },
+                    ],
+                    margin: "xl",
+                  },
+                  {
+                    type: "box",
+                    layout: "vertical",
+                    contents: [
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "Branch",
+                            size: "sm",
+                            color: "#858f89",
+                            flex: 3,
+                            margin: "md",
+                          },
+                          {
+                            type: "text",
+                            text: branch,
+                            size: "sm",
+                            margin: "md",
+                            flex: 5,
+                          },
+                        ],
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "Author",
+                            size: "sm",
+                            color: "#858f89",
+                            flex: 3,
+                            margin: "md",
+                          },
+                          {
+                            type: "text",
+                            text: authorName,
+                            size: "sm",
+                            margin: "md",
+                            flex: 5,
+                          },
+                        ],
+                        margin: "sm",
+                      },
+                      {
+                        type: "box",
+                        layout: "baseline",
+                        contents: [
+                          {
+                            type: "text",
+                            text: "Commit",
+                            size: "sm",
+                            color: "#858f89",
+                            flex: 3,
+                            margin: "md",
+                          },
+                          {
+                            type: "text",
+                            text: commitUrl.split("/").pop() || "",
+                            size: "sm",
+                            margin: "md",
+                            flex: 5,
+                            decoration: "underline",
+                            action: {
+                              type: "uri",
+                              label: "",
+                              uri: commitUrl,
+                            },
+                          },
+                        ],
+                        margin: "sm",
+                      },
+                    ],
+                    margin: "xxl",
+                    width: "260px",
+                    alignItems: "center",
+                    paddingAll: "6px",
+                  },
+                ],
+              },
+            },
+          ],
         },
-      };
-    }
+      },
+    };
+  }
 }
