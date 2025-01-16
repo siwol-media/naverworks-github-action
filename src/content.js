c74aa51a4b6c206a724442a6b1a521e8a7a49968
ariandel <ariandel@zohomail.com>
2025-01-16 15:32:02 +0900

    알림 메세지 내용변경

--- src/content.js
+++ src/content.js
@@ -1,6 +1,23 @@
 import github from "@actions/github";
 
 export default class Content {
+  /**
+   * @type {Object<string, string>}
+   */
+  authorMap = {
+    ariandel: "장원익",
+    "ariandel-dev": "장원익",
+    wldygus2023: "지요현",
+  };
+
+  /**
+   * @type {Object<string, string>}
+   */
+  branchMap = {
+    main: "실섭",
+    develop: "개발섭",
+  };
+
   constructor() {
     this.values = github.context;
   }
@@ -22,7 +39,9 @@ export default class Content {
     // repository 이름
     const repository = payload?.repository?.name || "unknown-repo";
     // 브랜치 이름 (refs/heads/main => main)
-    const branch = ref?.split("/").pop() || "unknown-branch";
+    const branchName = ref?.split("/").pop();
+
+    const branch = branchName ? (this.branchMap[branchName] || branchName) : "unknown-branch";
     // 작성자 이름 (GitHub username)
     const author = payload?.head_commit?.author?.username || payload?.head_commit?.author?.name || "unknown-author";
     // 커밋 메시지
@@ -37,131 +56,131 @@ export default class Content {
     return {
       content: {
         type: "flex",
-        altText: `[${repository}]\n ${commitMessage}`,
-        contents: {
-          type: "carousel",
-          contents: [
-            {
-              type: "bubble",
-              size: "mega",
-              header: {
-                type: "box",
-                layout: "horizontal",
-                contents: [
-                  {
-                    type: "text",
-                    text: repository,
-                    size: "lg",
-                    color: "#00c73c",
-                    weight: "bold",
-                  },
-                ],
-                backgroundColor: "#ffffff",
-              },
-              body: {
-                type: "box",
-                layout: "vertical",
-                contents: [
-                  {
-                    type: "box",
-                    layout: "vertical",
-                    contents: [
-                      {
-                        type: "text",
-                        text: commitMessage,
-                        wrap: true,
-                        size: "md",
-                      },
-                    ],
-                    margin: "xl",
-                  },
-                  {
-                    type: "box",
-                    layout: "vertical",
-                    contents: [
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "Branch",
-                            size: "sm",
-                            color: "#858f89",
-                            flex: 3,
-                            margin: "md",
-                          },
-                          {
-                            type: "text",
-                            text: branch,
-                            size: "sm",
-                            margin: "md",
-                            flex: 5,
-                          },
-                        ],
-                      },
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "Author",
-                            size: "sm",
-                            color: "#858f89",
-                            flex: 3,
-                            margin: "md",
-                          },
-                          {
-                            type: "text",
-                            text: authorName,
-                            size: "sm",
-                            margin: "md",
-                            flex: 5,
-                          },
-                        ],
-                        margin: "sm",
-                      },
-                      {
-                        type: "box",
-                        layout: "baseline",
-                        contents: [
-                          {
-                            type: "text",
-                            text: "Commit",
-                            size: "sm",
-                            color: "#858f89",
-                            flex: 3,
-                            margin: "md",
-                          },
-                          {
-                            type: "text",
-                            text: commitUrl.split("/").pop() || "",
-                            size: "sm",
-                            margin: "md",
-                            flex: 5,
-                            decoration: "underline",
-                            action: {
-                              type: "uri",
-                              label: "",
-                              uri: commitUrl,
+          altText: `[${repository}]\n ${commitMessage}`,
+          contents: {
+            type: "carousel",
+            contents: [
+              {
+                type: "bubble",
+                size: "mega",
+                header: {
+                  type: "box",
+                  layout: "horizontal",
+                  contents: [
+                    {
+                      type: "text",
+                      text: repository,
+                      size: "lg",
+                      color: "#00c73c",
+                      weight: "bold",
+                    },
+                  ],
+                  backgroundColor: "#ffffff",
+                },
+                body: {
+                  type: "box",
+                  layout: "vertical",
+                  contents: [
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "text",
+                          text: commitMessage,
+                          wrap: true,
+                          size: "md",
+                        },
+                      ],
+                      margin: "xl",
+                    },
+                    {
+                      type: "box",
+                      layout: "vertical",
+                      contents: [
+                        {
+                          type: "box",
+                          layout: "baseline",
+                          contents: [
+                            {
+                              type: "text",
+                              text: "브랜치",
+                              size: "sm",
+                              color: "#858f89",
+                              flex: 3,
+                              margin: "md",
                             },
-                          },
-                        ],
-                        margin: "sm",
-                      },
-                    ],
-                    margin: "xxl",
-                    width: "260px",
-                    alignItems: "center",
-                    paddingAll: "6px",
-                  },
-                ],
+                            {
+                              type: "text",
+                              text: branch,
+                              size: "sm",
+                              margin: "md",
+                              flex: 5,
+                            },
+                          ],
+                        },
+                        {
+                          type: "box",
+                          layout: "baseline",
+                          contents: [
+                            {
+                              type: "text",
+                              text: "작성자",
+                              size: "sm",
+                              color: "#858f89",
+                              flex: 3,
+                              margin: "md",
+                            },
+                            {
+                              type: "text",
+                              text: authorName,
+                              size: "sm",
+                              margin: "md",
+                              flex: 5,
+                            },
+                          ],
+                          margin: "sm",
+                        },
+                        {
+                          type: "box",
+                          layout: "baseline",
+                          contents: [
+                            {
+                              type: "text",
+                              text: "커밋",
+                              size: "sm",
+                              color: "#858f89",
+                              flex: 3,
+                              margin: "md",
+                            },
+                            {
+                              type: "text",
+                              text: commitUrl.split("/").pop() || "",
+                              size: "sm",
+                              margin: "md",
+                              flex: 5,
+                              decoration: "underline",
+                              action: {
+                                type: "uri",
+                                label: "",
+                                uri: commitUrl,
+                              },
+                            },
+                          ],
+                          margin: "sm",
+                        },
+                      ],
+                      margin: "xxl",
+                      width: "260px",
+                      alignItems: "center",
+                      paddingAll: "6px",
+                    },
+                  ],
+                },
               },
-            },
-          ],
+            ],
+          },
         },
-      },
-    };
-  }
+      };
+    }
 }
